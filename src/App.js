2377d4979ef41f6bed5d23ea14d98da29a190564
mekilan <mekilanravi2594@gmail.com>
2021-05-29 19:49:43 +0530

    secondcommit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Commentbox from './Commentbox';
 const App = () => {
   return (
-    <div className="ui container"><Commentbox /></div>
+    <div className="ui container">
+      <h2>Widget Commentbox</h2>
+      <Commentbox />
+    </div>
   );
 };
 export default App;
